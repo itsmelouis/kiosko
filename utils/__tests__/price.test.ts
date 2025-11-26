55acb61e8874dc0df0b5f0409cf2d2ef13bf5a40
Louis F. <me@itslouis.dev>
2025-11-26 14:10:15 +0100

    feat: remove empty test files as it triggers false positive tests

--- utils/__tests__/price.test.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Tests unitaires pour les calculs de prix
- * Obligatoire pour le workshop (minimum 2 tests unitaires)
- * TODO: Tester calculateTotal, calculateItemPrice, etc.
- */
--- utils/__tests__/validators.test.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Tests unitaires pour les validateurs
- * Obligatoire pour le workshop (minimum 2 tests unitaires)
- * TODO: Tester validateTableNumber, validateCardNumber, etc.
- */
