64d4e0bc5b40d6ef7094969a0c52a34269d2c3cc
Louis F. <me@itslouis.dev>
2025-11-26 14:06:21 +0100

    refactor(app): move all files into expo initial structure using MVVM architecture

--- src/screens/PaymentScreen.tsx
+++
@@ -1,5 +0,0 @@
-/**
- * Écran Paiement - US6: Paiement mocké
- * Simule un paiement par carte (faux formulaire)
- * TODO: Créer formulaire de paiement mock avec validation
- */
--- src/screens/ProductDetailsScreen.tsx
+++
@@ -1,5 +0,0 @@
-/**
- * Écran Détails Produit - US2: Consulter un produit + US3: Personnaliser
- * Affiche les détails, options de personnalisation, et bouton ajout au panier
- * TODO: Créer l'interface avec détails produit et sélecteur d'options
- */
